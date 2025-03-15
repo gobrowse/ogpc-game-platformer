039b057b75d165dddd00747900b23c70f05c189c
Mateo A <ogpcjam@gmail.com>
2025-03-15 11:08:24 -0700

    The main character can now climb up ladders.
    The main character can now climb up ladders. We also added some things, like now the characer is animated. Look at it closly, becouse it bearly moves.
    Github copilot helped with some line, but we had to debug it a lot.

--- src/game-ogpc.py
+++ src/game-ogpc.py
@@ -7,16 +7,16 @@ tk = Tk()
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.resizable(0,0)
-tk.wm_attributes("-topmost",1)
+#tk.wm_attributes("-topmost",1)
 tk.title("VERY GOOD GAME! Game")
-
-hero = graphics_ogpc.MainKid(canvas,0,390)
 my_ladder = graphics_ogpc.Ladder(canvas,250,340,280,390,4,5,'red')
-my_platform = graphics_ogpc.Platform(canvas,0,390,500,400,'peru')
+my_platform = graphics_ogpc.Platform(canvas,0,390,500,400,'blue')
+ladderlist = [my_ladder]
+hero = graphics_ogpc.MainKid(canvas,0,390,ladderlist)
 
 while True:
     hero.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.0001)
-    
+
--- src/graphics_ogpc.py
+++ src/graphics_ogpc.py
@@ -1,6 +1,11 @@
 from tkinter import *
+from PIL import Image, ImageTk
 class Ladder:
     def __init__(self, canvas, x1,y1,x2,y2,number_rungs, width, color):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
         self.Canvas = canvas
         self.side1 = canvas.create_rectangle(x1,y1,x1+width,y2, fill=color)
         self.side2 = canvas.create_rectangle(x2-width,y1,x2,y2, fill=color)
@@ -19,21 +24,38 @@ class Platform:
     def draw(self):
         pass
 class MainKid:
-    def __init__(self, canvas, x1,y1):
+    def __init__(self, canvas, x1,y1,ladderslist):
         self.canvas = canvas
+        self.ladderslist = ladderslist
         self.x1 = x1
         self.y1 = y1
-        self.photoimage = PhotoImage(file="../assets/images/person.gif")
-        self.image = canvas.create_image(x1,y1,image=self.photoimage,anchor="sw")
+#        self.photoimage = PhotoImage(file="../assets/images/person.gif")
+        self.photoimage = [PhotoImage(file="../assets/images/personframes/sprite_0.png"),PhotoImage(file="../assets/images/personframes/sprite_1.png"),PhotoImage(file="../assets/images/personframes/sprite_2.png"),PhotoImage(file="../assets/images/personframes/sprite_3.png"),PhotoImage(file="../assets/images/personframes/sprite_4.png"),PhotoImage(file="../assets/images/personframes/sprite_5.png")]
+        self.animate = 0
+        self.image = canvas.create_image(x1,y1,image=self.photoimage[self.animate],anchor="sw")
         canvas.bind_all("<KeyPress-Left>", self.move_left)
         canvas.bind_all("<KeyPress-Right>", self.move_right)
+        canvas.bind_all("<KeyPress-Up>", self.climb)
         self.xmove = 0
         self.ymove = 0
     def move_right(self,event):
+        self.animate = self.animate + 1
         self.xmove = 4
-    def move_left(self,event):
+    def move_left(self,event): 
+        self.animate = self.animate + 1
         self.xmove = -4
+    def climb(self,event):
+        if self.is_near_ladder():
+            self.ymove = -4
+    def is_near_ladder(self):
+        for x in self.ladderslist:
+            if self.x1 >= x.x1 and self.x1 <= x.x2:
+                return True
+        return False
     def draw(self):
+        if self.animate == 5:
+            self.animate = 0
+        self.canvas.itemconfig(self.image,image=self.photoimage[self.animate])
         if (self.x1 + self.xmove) <= 0:
             self.xmove = 0
         if (self.x1 + self.xmove) >= 450:
